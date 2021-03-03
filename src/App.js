9554dcb0985bba280fd0e1e2c82e582cecd0490b
FranElfers <fransozzan@gmail.com>
2021-03-03 08:12:43 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">
+          <h1>SocialStore app</h1>
+          <p>Hecho con React por Francisco Elfers</p>
+        </div>
       </header>
     </div>
   );
