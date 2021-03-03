a108e2a109d7388104c49c36eb233b98c373362b
FranElfers <fransozzan@gmail.com>
2021-03-03 09:42:44 -0300

    desafio 2 navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <div className="title">
           <h1>SocialStore app</h1>
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css';
+
+export default function NavBar() {
+	return <nav>
+		<p className="brand">SocialStore</p>
+		<div>
+			<p>🍔</p>
+			<p>🍾</p>
+			<p>👔</p>
+			<p>🚲</p>
+			<p>🎸</p>
+		</div>
+	</nav>
+}
